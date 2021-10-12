76af2b41bd6b2a47ce1023363ddacb7b0ceecf6c
Ashi Dubey <ashi.dubey@knoldus.com>
2021-10-13 00:10:14 +0530

    Testing branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,31 +1,39 @@
-pipeline {
-    agent{
-            label "slave-1"
-        }
-        tools {
-            maven 'maven'
-            // jdk 'jdk 11'
-        }
-    stages {
-
-        stage('Testing') {
-            steps {
-                echo 'Testing the application...'
-                sh "mvn clean test"
+pipeline{
+    // triggers{
+    //     pollSCM('H 0 * * 2')
+    // }
+    agent {label 'slave-2'}
+    tools { 
+        maven 'maven' 
+        jdk 'jdk 11' 
+    }
+    options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+    stages{
+        stage("cleaning"){
+            steps{
+                sh "mvn clean"
             }
         }
+        stage("testing"){
+            steps{
+                sh "mvn test"
+            }
+        }
+
     }
-    post {
-        always{
-            emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output at $BUILD_URL to view the results.''',
+    post{
+       always{
+            emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output and logs at -> $BUILD_URL ''',
             subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
             to: 'ashi.dubey@knoldus.com'
         }
         success{
-        echo "Testing stage successful"
+            echo "pipeline executed successfully"
         }
         failure{
-        echo "Testing stage failed"
+            echo "pipeline execution failed"
         }
     }
 }
