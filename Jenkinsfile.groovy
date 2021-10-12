3e7068c1cda0123f3edc44163ea7046f1de79468
Ashi Dubey <ashi.dubey@knoldus.com>
2021-10-12 23:44:15 +0530

    Development branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,24 +2,35 @@ pipeline{
     // triggers{
     //     pollSCM('H 0 * * 2')
     // }
-    agent{
-        label "slave-3"
-    }
+    agent {label 'slave-1'}
     tools { 
         maven 'maven' 
-        jdk 'jdk 11' 
+        jdk 'jdk11' 
     }
+    options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
     stages{
-        stage("building"){
+        stage("cleaning"){
             steps{
                 sh "mvn clean"
+            }
+        }
+        stage("compile"){
+            steps{
+                sh "mvn compile"
+            }
+        }
+        stage("packaging"){
+            steps{
+                sh 'mvn package'
             }
         }
 
     }
     post{
         always{
-            emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output at $BUILD_URL to view the results.''',
+            emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output and logs at -> $BUILD_URL ''',
             subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
             to: 'ashi.dubey@knoldus.com'
         }
