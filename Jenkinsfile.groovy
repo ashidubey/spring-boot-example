28db6dd222e4d276e5161a3b0944a522f7949c14
Ashi Dubey <ashi.dubey@knoldus.com>
2021-10-13 00:07:32 +0530

    Production branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,26 +1,44 @@
-pipeline {
-    agent {
-        label 'slave-2'
+pipeline{
+    // triggers{
+    //     pollSCM('H 0 * * 2')
+    // }
+    agent {label 'slave-3'}
+    tools { 
+        maven 'maven' 
+        jdk 'jdk 11' 
     }
-    stages {
-        stage("Production") {
-            steps {
-                //echo "Production branch"
+    options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+    stages{
+
+        stage("cleaning"){
+            steps{
                 sh "mvn clean"
             }
         }
+        stage("testing"){
+            steps{
+                sh "mvn test"
+            }
+        }
+        // stage("deploying"){
+        //     steps{
+        //         sh 'scp -i "/home/knoldus/Downloads/aws-auth.pem" /var/lib/jenkins/workspace/capstone-demo_development/target/hello-0.0.1-SNAPSHOT.jar ubuntu@15.206.147.135:/home/ubuntu'
+        //     }
+        // }
     }
-    post {
-        always{
-            emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output at $BUILD_URL to view the results.''',
+    post{
+       always{
+            emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output and logs at -> $BUILD_URL ''',
             subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
             to: 'ashi.dubey@knoldus.com'
         }
-        success {
-            echo "Packaging successful"
+        success{
+            echo "pipeline executed successfully"
         }
-        failure {
-            echo "Packaging unsuccessful"
+        failure{
+            echo "pipeline execution failed"
         }
     }
 }
