7fe1220cf8d7dc2a1444634a970ff9841131e161
Ashi Dubey <ashi.dubey@knoldus.com>
2021-10-12 15:28:47 +0530

    development branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline{
         always{
             emailext body:  '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output at $BUILD_URL to view the results.''',
             subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
-            to: 'rahul.soni@knoldus.com'
+            to: 'ashi.dubey@knoldus.com'
         }
         success{
             echo "pipeline executed successfully"
